d49541c9b8653be2f01b839ba9623c15c296e515
Philippe Moise <82619578+phildesignsgfx@users.noreply.github.com>
2024-10-08 21:13:14 +0000

    almost

--- src/app.js
+++ src/app.js
@@ -9,3 +9,16 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+document.addEventListener("DOMContentLoaded", () => {
+  let image = document.getElementById("image");
+  let content = document.getElementById("content");
+  let btnCreate = document.getElementById("btnCreate");
+
+  btnCreate.onclick = () => {
+    if (content.value === "") return;
+    let linkQR = "https://api.qrserver.com/v1/create-qr-code/";
+    image.src =
+      linkQR + "?size=500x500&data=" + encodeURIComponent(content.value);
+  };
+});
